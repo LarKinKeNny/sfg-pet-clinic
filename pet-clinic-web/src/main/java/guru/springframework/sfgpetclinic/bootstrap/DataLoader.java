c022d95e3de33d68d87ae9d78a372c33776ce885
leo.smits <lsmits.mail@gmail.com>
2021-12-14 13:25:13 +0200

    Added bootstrap for data. Closes #13.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,40 @@
+package guru.springframework.sfgpetclinic.bootstrap;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        var owner = new Owner();
+        owner.setId(1L);
+        owner.setFirstName("Micheal");
+        owner.setLastName("Weston");
+
+        ownerService.save(owner);
+
+        var vet = new Vet();
+        vet.setId(1L);
+        vet.setFirstName("Sam");
+        vet.setLastName("Axe");
+
+        vetService.save(vet);
+
+        System.out.println("finished loading bootstrap");
+    }
+}
