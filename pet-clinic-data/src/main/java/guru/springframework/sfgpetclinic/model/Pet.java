e28f506d1ca371b361ea87238cdf94f5260e5a0e
leo.smits <lsmits.mail@gmail.com>
2021-12-17 08:17:24 +0200

    Updated pet entity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -10,7 +11,7 @@ import java.util.Set;
 @Entity
 public class Pet extends BaseEntity {
 
-    @OneToMany(mappedBy = "pet")
+    @OneToMany(mappedBy = "pet", cascade = CascadeType.ALL)
     private Set<Visit> visits = new HashSet<>();
 
     @ManyToOne
