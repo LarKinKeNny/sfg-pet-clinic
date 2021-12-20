8051962cd544d2f9166b960acfb019a21b655780
leo.smits <lsmits.mail@gmail.com>
2021-12-20 08:41:09 +0200

    Created OwnerJpaService UT

---
+++ pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerJpaServiceTest.java
@@ -0,0 +1,76 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerJpaServiceTest {
+
+    @InjectMocks
+    OwnerJpaService ownerJpaService;
+
+    @Mock
+    OwnerRepository ownerRepository;
+
+    Owner owner;
+    private final Long id = 1L;
+
+    @BeforeEach
+    void setUp() {
+        owner = Owner.builder().id(id).build();
+    }
+
+    @Test
+    void findById() {
+        when(ownerRepository.findById(eq(id))).thenReturn(Optional.of(owner));
+        assertNotNull(ownerJpaService.findById(id));
+        verify(ownerRepository, times(1)).findById(anyLong());
+    }
+
+    @Test
+    void save() {
+        when(ownerRepository.save(any(Owner.class))).thenReturn(owner);
+        assertEquals(owner, ownerJpaService.save(owner));
+        verify(ownerRepository, times(1)).save(any());
+    }
+
+    @Test
+    void findAll() {
+        when(ownerRepository.findAll()).thenReturn(Set.of(owner));
+        assertFalse(ownerJpaService.findAll().isEmpty());
+        verify(ownerRepository, times(1)).findAll();
+    }
+
+    @Test
+    void delete() {
+        ownerJpaService.delete(owner);
+        verify(ownerRepository, times(1)).delete(any(Owner.class));
+    }
+
+    @Test
+    void deleteById() {
+        ownerJpaService.deleteById(id);
+        verify(ownerRepository, times(1)).deleteById(id);
+    }
+
+    @Test
+    void findByLastName() {
+        String lastName = "lastName";
+        ownerJpaService.findByLastName(lastName);
+        verify(ownerRepository, times(1)).findByLastName(eq(lastName));
+    }
+}
